2e1f63149053fdf7a14d998093863b822b06e4e3
philmervdm <philmer@ccilvn.be>
2016-06-03 12:08:40 +0200

    Add git ignore file

--- res_partner_zip/__openerp__.py
+++ res_partner_zip/__openerp__.py
@@ -2,7 +2,7 @@
 {
     'name': 'Zip code management',
     'summary':'Change the way the zip codes are handled :\nwith this module, tou define all possible zip codes and just link them to res.partner.\nEach zip code manage also it''s own country.',
-    'version':'1.0.0',
+    'version':'1.0.1',
     'author':'CCI Liege Verviers Namur',
     'category':'Customer Relationship Management',
     'depends': ['base',],
